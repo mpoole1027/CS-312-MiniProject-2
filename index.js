7d37827421ec1112dbe07cca80e1679b0410b0cd
Max Poole <mkp274@nau.edu>
2024-09-25 18:48:42 -0700

    api call and image display

--- index.js
+++ index.js
@@ -4,14 +4,31 @@ import axios from "axios";
 
 const app = express();
 const port = 3000;
-const API_URL = "www.thecocktaildb.com/api/json/v1/1/";
+const API_URL = "http://www.thecocktaildb.com/api/json/v1/1/";
+var result = null;
 
 app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
+app.get("/", (req, res) => {
+    res.render("index.ejs", { result: result });
+});
 
+app.get("/request", async (req, res) => {
+    try{
+        const response = await axios.get(`${API_URL}random.php`);
+        result = response.data;
+        //console.log(result);
+        res.redirect('/');
+    } catch (error) {
+        console.error("Failed to make request:", error.message);
+        res.render("index.ejs", {
+            error: error.message,
+        });
+    }
+});
 
 
 app.listen(port,() => {
